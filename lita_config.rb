8e7112144bbf405bcc1e646c50cd3ac5e6314b15
Tomohiro Murota <tomohiro.murota@gmail.com>
2015-12-19 21:15:17 +0900

    fix keep-alive

--- lita_config.rb
+++ lita_config.rb
@@ -7,10 +7,10 @@ Lita.configure do |config|
   # options for the Redis connection.
   config.redis[:url] = ENV['REDIS_URL']
 
-  # lita-keepalive
-  config.url = ENV['KEEPALIVE_URL']
-  config.minutes = 20
+  # lita-keepalive handler
+  config.handlers.keepalive.url = ENV['KEEPALIVE_URL']
+  config.handlers.keepalive.minutes = 20
 
-  # usatan adapter
+  # usatan handler
   config.handlers.usatan.dictionary.path = "naist-jdic"
 end
