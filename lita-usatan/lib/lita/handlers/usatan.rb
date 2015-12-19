723257d25f80ded0c23635bc57f11eab4daf46c8
Tomohiro Murota <tomohiro.murota@gmail.com>
2015-12-20 00:09:22 +0900

    modify usatan bot

--- lita-usatan/lib/lita/handlers/usatan.rb
+++ lita-usatan/lib/lita/handlers/usatan.rb
@@ -1,21 +1,71 @@
+require 'redis'
 require 'okura/serializer'
 
 module Lita
   module Handlers
     class Usatan < Handler
+      REDIS_KEY_DICT = 'usa:dictionary'
+      REDIS_KEY_LAST = 'usa:last'
       config :dictionary do
         config :path, type: String, required: true
       end
 
       route(/^(.+)/, :reply, help: { "SOMETHING" => "speak to usatan!!!" })
       def reply(response)
-        tagger = Okura::Serializer::FormatInfo.create_tagger(config.dictionary.path)
-        nodes = tagger.parse(response.matches[0][0])
-        nouns = nodes.mincost_path.select{|node| /^名詞/ =~ node.word.left.text}
-        response.reply(nouns.first.word.surface)
+        message = response.matches[0][0]
+
+        keyword = get_context
+        if keyword
+          reply = memorize(keyword, message)
+          response.reply(reply)
+          after(5) { |timer| response.reply(think(message)) }
+          return
+        end
+
+        response.reply(think(message))
       end
 
       Lita.register_handler(self)
+
+      private
+      def get_context
+        keyword = Lita.redis.get(REDIS_KEY_LAST)
+        Lita.redis.del(REDIS_KEY_LAST)
+        keyword
+      end
+
+      def ask(keyword)
+        Lita.redis.set(REDIS_KEY_LAST, keyword)
+        "#{keyword}ってなーに?"
+      end
+
+      def remember(keyword)
+        Lita.redis.hget(REDIS_KEY_DICT, keyword)
+      end
+
+      def memorize(keyword, meaning)
+        Lita.redis.hset(REDIS_KEY_DICT, keyword, meaning)
+        ['ふーん', 'へぇ〜', 'なるほどぉ', 'ありがと！'].sample
+      end
+
+      def think(message)
+        words = get_nouns(message)
+        if words.any?
+          words.each do |word|
+            if (meaning = remember(word))
+              return "それ知ってる！#{meaning}、でしょ？！"
+            end
+          end
+          ask(words.sample)
+        end
+        ['うん？', 'へっ？', 'ふーん', 'そなんだー'].sample
+      end
+
+      def get_nouns(message)
+        tagger = Okura::Serializer::FormatInfo.create_tagger(config.dictionary.path)
+        nodes = tagger.parse(message)
+        nodes.mincost_path.select{|node| /^名詞/ =~ node.word.left.text}.map{|node| node.word.surface}
+      end
     end
   end
 end
