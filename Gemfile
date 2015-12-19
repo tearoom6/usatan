94923c7670b4ecb66c501c3e0c3fec760f4babb8
Tomohiro Murota <tomohiro.murota@gmail.com>
2015-12-19 11:37:45 +0900

    prepare to deploy Heroku

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
+ruby "2.1.8"
 
 gem "lita"
 gem "lita-slack"
+gem "redis"
 gem "lita-usatan", path: "lita-usatan"
 
--- lita_config.rb
+++ lita_config.rb
@@ -4,9 +4,8 @@ Lita.configure do |config|
   config.adapters.slack.token = ENV['LITA_SLACK_TOKEN']
   config.robot.admins = ["U0EHUPSH4"]
 
-  ## Set options for the Redis connection.
-  # config.redis.host = "127.0.0.1"
-  # config.redis.port = 1234
+  # options for the Redis connection.
+  config.redis[:url] = ENV['REDIS_URL']
 
   # usatan adapter
   config.handlers.usatan.dictionary.path = "naist-jdic"
